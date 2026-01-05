06323e382c168fdf328d010f245876b1f5da5d7c
ShelfofSheElfs <reirodneyjohnson@gmail.com>
2026-01-04 21:29:33 -0500

    Preliminary test data, 99% accurate @ 1.033k files Roughly 37 files were repeated intentionally Repeat files showed same outcomes, so training is consistent
    Terminal output available in attached text file

--- detection.py
+++ detection.py
@@ -15,17 +15,34 @@ import time
 import numpy as np
 from astropy.io import fits
 from scipy.special import kl_div
+from matplotlib import pyplot as plt
 
 # Logging Function
-def logs():
+def logs(score):
     global tot_fcount
     global c
     global pw
     global start
+    global fails
+    global false_positives
+    global false_negatives
+    global passes
+    global t_passes
     pw+=1
     os.system("clear")
-    print("Process Watchdog:", pw, f"~{round((pw/(7+tot_fcount))*100, 2)}%", "\nReq. Files:", tot_fcount-c)
-    print("Total Runtime:", round((time.time()-start)/60, 2), "m")
+    if score == False:
+        print("Process Watchdog:", pw, f"~{round((pw/(7+tot_fcount))*100, 2)}%", "\nReq. Files:", tot_fcount-c)
+        print("Total Runtime:", round((time.time()-start)/60, 2), "m")
+    else:
+        print(f"Total Passes   : {passes}")
+        print(f"Total Failures : {fails}")
+        print(f"Passed Transits: {t_passes}")
+        print(f"Failed Transits: {false_negatives}")
+        print(f"False Positives: {false_positives}")
+        print(f"Accuracy Rate  : {passes/(passes+fails)}")
+        print(f"Progress       : {(passes+fails)/tot_fcount}\n")
+        print("Total Runtime   :", round((time.time()-start)/60, 2), "m")
+
 
 def find_cache(fil):
     if os.path.exists(f"venv/{fil}cache.npy"):
@@ -48,40 +65,39 @@ def get_bases():
             avgt = np.sum(transits, axis=0)/len(transits)
             np.save("venv/avgtcache.npy", (np.sum(transits, axis=0)/len(transits)))
             np.save("venv/transitcache.npy", transits)
+            logs(False)
+            print("AVGt CACHE SAVED")
         else:
             transits = find_cache("transit")
             avgt = np.load("venv/avgtcache.npy")
-            tot_fcount = 0
-            logs()
-            print("AVGt DONE")
+            logs(False)
+            print("AVGt CACHE PULLED")
     except ValueError:
         transits = np.load("venv/transitcache.npy")
         avgt = np.load("venv/avgtcache.npy")
-        tot_fcount = 0
-        logs()
-        print("AVGt DONE")
+        logs(False)
+        print("AVGt CACHE PULLED")
 
-    bases = 0
     try:
         if find_cache("base") == False:
             for support in os.listdir("FITS/Baseline"):
                 supports.append(fits.getdata(f"FITS/Baseline/{support}").astype("<i4"))
-                c+=1; logs()
+                c+=1; logs(False)
             avgs = np.sum(supports, axis=0)/len(supports)
             np.save("venv/avgscache.npy", (np.sum(supports, axis=0)/len(supports)))
             np.save("venv/basecache.npy", supports)
+            logs(False)
+            print("AVGs CACHE SAVED")
         else:
             supports = np.load("venv/basecache.npy")
             avgs = np.load("venv/avgscache.npy")
-            tot_fcount = 0
-            logs()
-            print("AVGs DONE")
+            logs(False)
+            print("AVGs CACHE PULLED")
     except ValueError:
         supports = np.load("venv/basecache.npy")
         avgs = np.load("venv/avgscache.npy")
-        tot_fcount = 0
-        logs()
-        print("AVGs DONE")
+        logs(False)
+        print("AVGs CACHE PULLED")
 
     return avgt, avgs
 
@@ -89,10 +105,18 @@ def get_bases():
 def train_data(tbase, bbase):
     # Get Global Vars
     global tot_fcount
+    global start
+    global fails
+    global false_positives
+    global false_negatives
+    global passes
+    global t_passes
 
     num = 0
     tbase = (tbase-np.min(tbase))/(np.max(tbase)-np.min(tbase))
     bbase = (bbase-np.min(bbase))/(np.max(bbase)-np.min(bbase))
+
+    
 
     # Training Data
     for trainer in os.listdir("FITS/Training"):
@@ -112,24 +136,35 @@ def train_data(tbase, bbase):
         values, bins, _ = plt.hist(baseline_kld)
         barea = np.sum(np.diff(bins)*values)
 
-        if barea > tarea:
-            print(trainer)
+        if barea > tarea and trainer[0]=="T":
+            t_passes+=1
+            passes+=1
+            logs(True)
+        elif barea > tarea and trainer[0]!="T":
+            false_positives+=1
+            fails+=1
+            logs(True)
+        elif tarea > barea and trainer[0]=="T":
+            false_negatives+=1
+            fails+=1
+            logs(True)
         else:
-            print("No Pass")
+            passes+=1
+            logs(True)
 
 # Timer/Progress Track
 start = time.time()
 c=0
 pw=0
+fails = 0
+false_positives = 0
+false_negatives = 0
+passes = 0
+t_passes = 0
 
 # Data Counts
-transit_fcount = len([name for name in os.listdir('FITS/Transit/')])
-baseline_fcount = len([name for name in os.listdir('FITS/Baseline/')])
-unlabeled_fcount = len([name for name in os.listdir('FITS/Unlabeled/')])
 train_fcount = len([name for name in os.listdir('FITS/Training')])
-tot_fcount = transit_fcount+baseline_fcount+unlabeled_fcount+train_fcount
+tot_fcount = train_fcount
 
 avgt, avgs = get_bases()
 train_data(avgt, avgs)
-
-logs()
